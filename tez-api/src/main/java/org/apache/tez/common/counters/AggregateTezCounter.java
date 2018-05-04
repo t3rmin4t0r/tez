804b5bae5ae7b43a8da1def938a999a70e170de2
Gopal V <gopalv@apache.org>
2018-05-04 16:31:01 -0700

    WIP

---
+++ tez-api/src/main/java/org/apache/tez/common/counters/AggregateTezCounter.java
@@ -0,0 +1,78 @@
+package org.apache.tez.common.counters;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AggregateTezCounter extends AbstractCounter {
+
+  private final TezCounter child;
+  private long min = Long.MAX_VALUE;
+  private long max = Long.MIN_VALUE;
+
+  public AggregateTezCounter(TezCounter child) {
+    this.child = child;
+  }
+  
+  @Override
+  public String getName() {
+    return String.format("%s_aggregate", child.getName());
+  }
+
+  @Override
+  public String getDisplayName() {
+    return String.format("Aggregate(%s)", child.getDisplayName());
+  }
+
+  @Override
+  public long getValue() {
+    return child.getValue();
+  }
+
+  @Override
+  public void setValue(long value) {
+    this.child.setValue(value);
+  }
+
+  @Override
+  public void increment(long incr) {
+    throw new IllegalArgumentException("Cannot increment an aggregate counter");
+  }
+  
+  @Override 
+  public synchronized void aggregate(TezCounter other) {
+    final long val = other.getValue();
+    this.child.increment(val);
+    if (this.min == Long.MAX_VALUE) {
+      this.min = this.max = other.getValue();
+      return;
+    }
+    this.min = Math.min(this.min, val);
+    this.max = Math.max(this.max, val);
+  }
+
+  @Override
+  public TezCounter getUnderlyingCounter() {
+    return this.child;
+  }
+
+  @Override
+  public void readFields(DataInput arg0) throws IOException {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void write(DataOutput arg0) throws IOException {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public synchronized long getMin() {
+    return min;
+  }
+  
+  public synchronized long getMax() {
+    return max;
+  }
+}
---
+++ tez-api/src/main/java/org/apache/tez/common/counters/AggregateTezCounters.java
@@ -0,0 +1,101 @@
+package org.apache.tez.common.counters;
+
+
+public class AggregateTezCounters extends TezCounters {
+  
+  private static final GroupFactory groupFactory = new GroupFactory();
+  
+  public AggregateTezCounters() {
+    super(groupFactory);
+  }
+  
+  // Mix framework group implementation into CounterGroup interface
+  private static class AggregateFrameworkGroupImpl<T extends Enum<T>>
+      extends FrameworkCounterGroup<T, TezCounter> implements CounterGroup {
+
+    AggregateFrameworkGroupImpl(Class<T> cls) {
+      super(cls);
+    }
+
+    @Override
+    protected FrameworkCounter<T> newCounter(T key) {
+      return new FrameworkCounter<T>(key, getName());
+    }
+
+    @Override
+    public CounterGroupBase<TezCounter> getUnderlyingGroup() {
+      return this;
+    }
+  }
+
+  // Mix generic group implementation into CounterGroup interface
+  // and provide some mandatory group factory methods.
+  private static class AggregateGenericGroup extends AbstractCounterGroup<TezCounter>
+      implements CounterGroup {
+
+    AggregateGenericGroup(String name, String displayName, Limits limits) {
+      super(name, displayName, limits);
+    }
+
+    @Override
+    protected TezCounter newCounter(String name, String displayName, long value) {
+      return new GenericCounter(name, displayName, value);
+    }
+
+    @Override
+    protected TezCounter newCounter() {
+      return new GenericCounter();
+    }
+
+    @Override
+    public CounterGroupBase<TezCounter> getUnderlyingGroup() {
+      return this;
+    }
+  }
+
+  // Mix file system group implementation into the CounterGroup interface
+  private static class AggregateFileSystemGroup extends FileSystemCounterGroup<TezCounter>
+      implements CounterGroup {
+
+    @Override
+    protected TezCounter newCounter(String scheme, FileSystemCounter key) {
+      return new FSCounter(scheme, key);
+    }
+
+    @Override
+    public CounterGroupBase<TezCounter> getUnderlyingGroup() {
+      return this;
+    }
+  }
+
+  /**
+   * Provide factory methods for counter group factory implementation.
+   * See also the GroupFactory in
+   *  {@link org.apache.hadoop.TezCounters.Counters mapred.Counters}
+   */
+  private static class GroupFactory
+      extends CounterGroupFactory<TezCounter, CounterGroup> {
+
+    @Override
+    protected <T extends Enum<T>>
+    FrameworkGroupFactory<CounterGroup>
+        newFrameworkGroupFactory(final Class<T> cls) {
+      return new FrameworkGroupFactory<CounterGroup>() {
+        @Override public CounterGroup newGroup(String name) {
+          return new AggregateFrameworkGroupImpl<T>(cls); // impl in this package
+        }
+      };
+    }
+
+    @Override
+    protected CounterGroup newGenericGroup(String name, String displayName,
+                                           Limits limits) {
+      return new AggregateGenericGroup(name, displayName, limits);
+    }
+
+    @Override
+    protected CounterGroup newFileSystemGroup() {
+      return new AggregateFileSystemGroup();
+    }
+  }
+}
